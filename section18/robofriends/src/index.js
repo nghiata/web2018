0abbced7501704efc9c7cd18f766422f21aa7a77
nghia ta <congnghia.ta@gmail.com>
2018-04-06 14:37:44 +0700

    update robot friend

--- section18/robofriends/src/index.js
+++ section18/robofriends/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'nghiatc'} />, document.getElementById('root'));
 registerServiceWorker();
