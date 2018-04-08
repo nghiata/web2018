908aceb2b98eb85be1f041431f5b3324b70d1db9
nghia ta <info@funaxy.com>
2018-04-08 13:42:52 +0700

    update robofriend

---
+++ section18/robofriends/src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        // <h1>Robofriends</h1>
+        <div className="bg-light-green dib br3 pa3 ma2 grow">
+            <img src="https://robohash.org/test?200x200" alt="robots"/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- section18/robofriends/src/index.js
+++ section18/robofriends/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Hello greeting={'nghiatc'} />, document.getElementById('root'));
+// ReactDOM.render(<Hello greeting={'nghiatc'} />, document.getElementById('root'));
+ReactDOM.render(
+                <div>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+                , 
+document.getElementById('root'));
 registerServiceWorker();
