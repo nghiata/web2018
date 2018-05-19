860e90b8145b59540d237b7eb6c3429de4dbbf70
nghia ta <congnghia.ta@gmail.com>
2018-05-19 15:53:34 +0700

    update robots. filter name

--- section18/robofriends/src/App.js
+++ section18/robofriends/src/App.js
@@ -12,16 +12,20 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event) {
-        console.log(event.target.value)
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })        
     }
 
     render() {
+        const filterRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        });
+        // console.log(filterRobots);
         return (
             <div className='tc'>
                 <h1>Robot Friends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots} />
+                <CardList robots={filterRobots} />
             </div>
         )
     }
