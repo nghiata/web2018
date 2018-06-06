993af9436b4678d6f2d82df5fa6f5c68d9f5f518
nghia ta <congnghia.ta@gmail.com>
2018-06-06 13:27:53 +0700

    update learning

--- section10/script.js
+++ section10/script.js
@@ -1,11 +1,48 @@
-var user = {
-	'name': 'nghiata',
-	'age': 28,
-	'hobby': 'swimming',
-	'isMarried': false,
-	'spells': ['abrakadra', 'shazam', 'boo'],
-	'shout': function() {
-		console.log('AHHHH!');
+var database = [
+	{
+		'username': 'nghiata',
+		'password': '123456'
+	},
+	{
+		'username': 'John',
+		'password': '123'
+	},
+	{
+		'username': 'Peter',
+		'password': '13579'
 	}
+];
+
+var newsfeed = [
+	{
+		'username': 'John',
+		'timeline': 'Javascript is so crazy'
+	},
+	{
+		'username': 'Peter',
+		'timeline': 'I love javascript'
+	},
+];
+
+var usernamePrompt = prompt("What\'s your name?");
+var passwordPrompt = prompt("What\'s your password?");
+
+function isUserValid(username, password) {
+	for (var i=0; i<database.length; i++) {
+		if (username == database[i].username && password == database[i].password) {
+			return true;
+		}		
+	}
+	return false;
 }
 
+function signIn(username, password) {
+	if (isUserValid(username, password)) {
+		console.log(newsfeed)
+	}
+	else {
+		alert('Sorry, wrong username and password');
+	}	
+}
+
+signIn(usernamePrompt, passwordPrompt);
